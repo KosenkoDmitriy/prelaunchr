9384b7228198f81bb60a0c699f70c27db014d8bd
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2021-04-27 23:53:39 +0300

    change refer page text content

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,28 +16,28 @@ class User < ActiveRecord::Base
   REFERRAL_STEPS = [
     {
       'count' => 5,
-      'html' => 'Shave<br>Cream',
+      'html' => 'Free baby grow<br>that makes 2 free baby grows (worth £49.90) with your first order!)',
       'class' => 'two',
       'image' =>  ActionController::Base.helpers.asset_path(
         'refer/cream-tooltip@2x.png')
     },
     {
       'count' => 10,
-      'html' => 'Truman Handle<br>w/ Blade',
+      'html' => "3 month’s worth of free <br> baby grows",
       'class' => 'three',
       'image' => ActionController::Base.helpers.asset_path(
         'refer/truman@2x.png')
     },
     {
       'count' => 25,
-      'html' => 'Winston<br>Shave Set',
+      'html' => "6 month’s worth of free <br> baby grows",
       'class' => 'four',
       'image' => ActionController::Base.helpers.asset_path(
         'refer/winston@2x.png')
     },
     {
       'count' => 50,
-      'html' => 'One Year<br>Free Blades',
+      'html' => "12 month’s worth of free <br> baby grows",
       'class' => 'five',
       'image' => ActionController::Base.helpers.asset_path(
         'refer/blade-explain@2x.png')
